33d6ded047e9a50dfff857266ec4e35e6cecb0aa
bram <eckelradekip10@gmail.com>
2021-09-20 13:51:36 +0100

    Added show message properly

---
+++ show_message.py
@@ -0,0 +1,28 @@
+import argparse
+
+from utility import str_to_color
+
+
+def show_message(sense, cmd_arguments):
+    parser = get_arg_parser()
+    argument_class = parser.parse_args(cmd_arguments, parser)
+    sense.show_message(argument_class.message, argument_class.text_speed,
+                       argument_class.text_color, argument_class.back_color)
+
+
+def get_arg_parser():
+    parser = argparse.ArgumentParser("Show a display message:")
+    parser.add_argument("message")
+    parser.add_argument("-s", "--text_speed", type=float, default=0.1)
+    parser.add_argument("-c", "--text_color", type=str_to_color, default="255-255-255")
+    parser.add_argument("-bc", "--back_color", type=str_to_color, default="0-0-0")
+    return parser
+
+
+def _show_message(sense, message, speed, text_color, back_color):
+    sense.show_message(message, speed, text_color, back_color)
+    sense.clear()
+
+
+if __name__ == "__main__":
+    main()
